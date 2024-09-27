04288041c6dd79140734c7d2f8246a67a7dbbb2c
3illBaby <thrillanon@outlook.com>
2024-09-27 08:49:31 +0100

    chore(page.tsx): added toggel component

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,17 @@
 import React from "react";
+import ThemeToggle from "@/app/components/theme-toggle";
 
 const Page = () => {
   return (
-    <main>
-      <h1
-        className={`flex justify-center self-center font-helvetica text-3xl font-extrabold tracking-widest text-gray-200`}
-      >
-        DARKSKINALIEN
-      </h1>
+    <main className={`flex h-full w-full flex-col px-3`}>
+      <div className={`flex flex-row items-center justify-between`}>
+        <h1
+          className={`flex self-center font-helvetica text-3xl font-extrabold tracking-widest text-gray-950 dark:text-gray-200`}
+        >
+          DARKSKINALIEN
+        </h1>
+        <ThemeToggle />
+      </div>
     </main>
   );
 };
