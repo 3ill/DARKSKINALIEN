f82ac5d3c1d0d0dcea171db9694c4da0db6f99d9
3illBaby <thrillanon@outlook.com>
2024-10-02 22:05:56 +0100

    chore(assets): added navigation assets
    added nav assets for light and dark theme

---
+++ app/assets/index.ts
@@ -0,0 +1,23 @@
+import instagram_dark from "./icons/dark/Instagram (1).png";
+import instagram_light from "./icons/light/Instagram.png";
+import twitter_dark from "/icons/dark/TwitterX.png";
+import twitter_light from "/icons/light/TwitterX.png";
+import linkedin_dark from "/icons/dark/LinkedIn.png";
+import linkedin_light from "/icons/light/LinkedIn.png";
+import sun from "/icons/light/Sun.png";
+import moon from "/icons/dark/Moon Symbol.png";
+import dark_logo from "/icons/dark/logo.png";
+import light_logo from "/icons/light/logo_black.png";
+
+export {
+  instagram_light,
+  instagram_dark,
+  twitter_light,
+  twitter_dark,
+  linkedin_light,
+  linkedin_dark,
+  sun,
+  moon,
+  dark_logo,
+  light_logo,
+};
