d47786a12f2182e9996e7486c02656dc3a889f01
3illBaby <thrillanon@outlook.com>
2024-10-03 23:05:56 +0100

    chore(assets): added new assets

--- app/assets/index.ts
+++ app/assets/index.ts
@@ -8,6 +8,10 @@ import sun from "./icons/light/Sun.png";
 import moon from "./icons/dark/Moon Symbol.png";
 import dark_logo from "./icons/dark/logo.png";
 import light_logo from "./icons/light/logo_black.png";
+import menu_close_dark from "./icons/dark/menu_close_dark.png";
+import menu_open_dark from "./icons/dark/menu_open_dark.png";
+import menu_close_light from "./icons/light/menu_close_light.png";
+import menu_open_light from "./icons/light/menu_open_light.png";
 
 export {
   instagram_light,
@@ -20,4 +24,8 @@ export {
   moon,
   dark_logo,
   light_logo,
+  menu_open_dark,
+  menu_close_dark,
+  menu_open_light,
+  menu_close_light,
 };
