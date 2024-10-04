c10213128e8fd175d3f7140d61a4d6c39beb9352
3illBaby <thrillanon@outlook.com>
2024-10-04 23:56:53 +0100

    style(page.tsx): made nav div full height

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Nav from "@/app/components/nav";
+import Footer from "@/app/components/footer";
 
 const Page = () => {
   return (
-    <main className={`flex h-full w-full flex-col px-3`}>
-      <div className={` w-full`}>
+    <main className={`flex h-full w-full flex-col`}>
+      <div className={` w-full px-3 h-screen`}>
         <Nav />
       </div>
+      <Footer />
     </main>
   );
 };
