74af330346cb27f7d463b3b1939516017e18cec6
3illBaby <thrillanon@outlook.com>
2024-10-06 22:00:50 +0100

    style(nav-slider.tsx): increase slider height

--- app/components/nav-slider.tsx
+++ app/components/nav-slider.tsx
@@ -3,11 +3,11 @@ import React from "react";
 const NavSlider = () => {
   return (
     <div
-      className={`flex h-9 w-5 items-center justify-center rounded-[25px] border border-white bg-black bg-opacity-50 sm:h-[80px] sm:w-10`}
+      className={`flex h-[70px] w-10 items-center justify-center rounded-[25px] border border-white bg-black bg-opacity-50 sm:h-[80px] sm:w-10`}
     >
       <a href={`#about`}>
         <div
-          className={`flex h-3 w-3 animate-bounce self-center rounded-full bg-white duration-1000 sm:h-5 sm:w-5`}
+          className={`flex h-5 w-5 animate-bounce self-center rounded-full bg-white duration-1000 sm:h-5 sm:w-5`}
         />
       </a>
     </div>
