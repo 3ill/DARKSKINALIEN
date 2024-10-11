7adc9ae66d0238fff4605ba9431720569bd0a6a2
3illBaby <thrillanon@outlook.com>
2024-10-11 09:41:41 +0100

    chore(hero.tsx): added nav slider

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -2,11 +2,15 @@
 
 import React from "react";
 import { motion } from "framer-motion";
-import { staggerContainer } from "@/app/utils/motion";
+import { softVariant, staggerContainer } from "@/app/utils/motion";
 import Image from "next/image";
 import { alien } from "../assets";
+import { useTheme } from "../context/theme.context";
+import { HeroSubText } from "../data/constants";
+import NavSlider from "./nav-slider";
 
 const Hero = () => {
+  const { theme } = useTheme();
   return (
     <motion.section
       variants={staggerContainer(0.1, 0.1)}
@@ -29,31 +33,47 @@ const Hero = () => {
             >
               Alien
             </h1>
-            <div className={`glow flex flex-row gap-2`}>
+
+            {/**Hero SubText */}
+            <motion.div
+              variants={softVariant}
+              initial={`hidden`}
+              whileInView={`visible`}
+              className={`${theme === "dark" ? "glow" : "dark_glow"} flex flex-row gap-2`}
+            >
               <div
                 className={`h-full w-1 rounded-md bg-primary_dark dark:bg-primary_light`}
               />
-              <div className={`flex flex-col sm:text-[18px] lg:text-[20px]`}>
-                <p className={`text_variant2 font-Semibold font-Azeret`}>
-                  Founder
-                </p>
-                <p className={`text_variant2 font-Semibold font-Azeret`}>
-                  Leader
-                </p>
-                <p className={`text_variant2 font-Semibold font-Azeret`}>
-                  Community Engr
-                </p>
+              <div className={`flex flex-col`}>
+                {HeroSubText.map((item) => (
+                  <div key={item.id} className={`flex flex-col`}>
+                    <p
+                      className={`text_variant2 font-Semibold font-Azeret capitalize`}
+                    >
+                      {item.name}
+                    </p>
+                  </div>
+                ))}
               </div>
-            </div>
+            </motion.div>
           </div>
 
+          {/**Hero Image: Right */}
           <div className={`relative flex h-full w-full flex-col`}>
             <Image
               src={alien}
               alt="alien"
               quality={100}
-              className={`absolute -right-[60px] -top-[90px] h-[450px] w-[500px] self-end object-contain object-center sm:-right-[130px] sm:-top-[350px] sm:h-[600px] sm:w-[600px] lg:-top-[80px] lg:h-[850px] lg:w-[900px]`}
+              className={`absolute -right-[60px] -top-[90px] -z-50 h-[450px] w-[500px] self-end object-contain object-center sm:-right-[130px] sm:-top-[350px] sm:h-[600px] sm:w-[600px] lg:-top-[80px] lg:h-[850px] lg:w-[900px]`}
             />
+          </div>
+        </div>
+        {/**Nav Slider */}
+        <div
+          className={`relative mt-[270px] flex w-full flex-col items-center justify-center sm:mt-[150px] lg:-mt-[30px]`}
+        >
+          <div>
+            <NavSlider />
           </div>
         </div>
       </section>
