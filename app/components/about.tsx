a706ebf0ddc4dc7582f070dca978ee3dca483502
3illBaby <thrillanon@outlook.com>
2024-10-12 22:16:17 +0100

    chore(about.tsx): added title

--- app/components/about.tsx
+++ app/components/about.tsx
@@ -1,8 +1,11 @@
 import React from "react";
+import Title from "./title";
 
 const About = () => {
   return (
-    <div className={`h-screen`}>About</div>
+    <section id="about" className={`flex h-screen w-full pt-0 sm:pt-[150px]`}>
+      <Title title={`About Me`} />
+    </section>
   );
 };
 export default About;
