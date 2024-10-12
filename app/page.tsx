c47f38975a9badba9e97568ad00a43d2d1fc1ab3
3illBaby <thrillanon@outlook.com>
2024-10-12 22:16:36 +0100

    chore(page.tsx): added about component

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,17 @@ import React from "react";
 import Nav from "@/app/components/nav";
 import Footer from "@/app/components/footer";
 import Hero from "@/app/components/hero";
+import About from "./components/about";
 
 const Page = () => {
   return (
-    <main className={`flex h-full w-full flex-col overflow-y-hidden overflow-x-hidden`}>
-      <div className={` w-full px-3 overflow-x-hidden overflow-y-hidden`}>
+    <main
+      className={`flex h-full w-full flex-col overflow-x-hidden overflow-y-hidden`}
+    >
+      <div className={`w-full overflow-x-hidden overflow-y-hidden px-3`}>
         <Nav />
         <Hero />
+        <About />
       </div>
       <Footer />
     </main>
