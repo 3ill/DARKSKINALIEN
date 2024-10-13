a325cd127c57481361c5e223f0534681a1915af4
3illBaby <thrillanon@outlook.com>
2024-10-13 21:10:39 +0100

    chore(body.tsx): created body component
    added motion to body text and marked as use client

---
+++ app/components/body.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import React from "react";
+import { motion } from "framer-motion";
+import { softVariant, staggerContainer } from "../utils/motion";
+
+interface Props {
+  children: React.ReactNode;
+}
+
+const Body = ({ children }: Props) => {
+  return (
+    <motion.section
+      variants={staggerContainer(0.1, 0.1)}
+      initial={`hidden`}
+      whileInView={`show`}
+      viewport={{ once: false, amount: 0.75 }}
+      className={`flex w-full flex-col`}
+    >
+      <motion.div
+        variants={softVariant}
+        initial={`hidden`}
+        whileInView={`visible`}
+      >
+        {children}
+      </motion.div>
+    </motion.section>
+  );
+};
+
+export default Body;
