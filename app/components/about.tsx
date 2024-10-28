0a6f9287c85d29d2ba9d34490045f72b3371024b
3illBaby <thrillanon@outlook.com>
2024-10-28 20:49:01 +0100

    revert(about.tsx): removed span

--- app/components/about.tsx
+++ app/components/about.tsx
@@ -15,30 +15,17 @@ const About = () => {
           <p
             className={`body_text_variant font-helvetica text-[14px] font-light sm:text-[16px]`}
           >
-            Hi, I&apos;m{" "}
-            <span className={`text-black dark:text-white`}>Martins Victor</span>
-            , though some know me as{" "}
-            <span className={`text-black dark:text-white`}>DarkSkinAlien</span>.
-            I{" "}
-            <span className={`text-black dark:text-white`}>
-              started DSA CORP
-            </span>{" "}
-            to bring people together through learning, teamwork, and personal
-            growth. My focus is on{" "}
-            <span className={`text-black dark:text-white`}>
-              building strong communities and helping people understand the
-              potential of Web3 technology
-            </span>
-            , especially for creatives across Africa. <br /> <br /> At DSA CORP,
-            I work on projects that encourage creativity and personal
-            development, giving people the tools to explore new ideas and grow
-            beyond what they thought possible.{" "}
-            <span className={`text-black dark:text-white`}>
-              My goal is straightforward: to open doors for future thinkers,
-              offer support to creative minds, and inspire people to achieve
-              their best in a connected, digital world
-            </span>
-            .
+            Hi, I&apos;m Martins Victor , though some know me as DarkSkinAlien I{" "}
+            started DSA CORP to bring people together through learning,
+            teamwork, and personal growth. My focus is on building strong
+            communities and helping people understand the potential of Web3
+            technology , especially for creatives across Africa. <br /> <br />
+            At DSA CORP, I work on projects that encourage creativity and
+            personal development, giving people the tools to explore new ideas
+            and grow beyond what they thought possible. My goal is
+            straightforward: to open doors for future thinkers, offer support to
+            creative minds, and inspire people to achieve their best in a
+            connected, digital world .
           </p>
         </Body>
 
