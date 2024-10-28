766c8fd21b74229ade24d3c7f3938e4260d7f8ce
3illBaby <thrillanon@outlook.com>
2024-10-28 12:56:11 +0100

    refactor(about.tsx): refactored about text

--- app/components/about.tsx
+++ app/components/about.tsx
@@ -15,17 +15,18 @@ const About = () => {
           <p
             className={`body_text_variant font-helvetica text-[14px] font-light sm:text-[16px]`}
           >
-            Hi, I&apos;m Martins Victor, but most people know me as
-            DarkSkinAlien. I&apos;m a passionate African creative on a mission
-            to change the narrative for African innovators and creators,
-            especially within the Web 3.0 ecosystem. <br /> <br /> My journey
-            has been one of uncertainty and growth, but through it all,
-            I&apos;ve developed a deep belief in the transformative power of
-            collaboration. <br /> <br /> From community building to management,
-            I&apos;ve found my calling in helping others unlock their potential
-            and thrive in this new digital frontier. I&apos;m here to create,
-            connect, and drive change. Let&apos;s build something amazing
-            together.
+            Hi, I&apos;m Martins Victor, though some know me as DarkSkinAlien. I
+            started DSA CORP to bring people together through learning,
+            teamwork, and personal growth. My focus is on building strong
+            communities and helping people understand the potential of Web3
+            technology, especially for creatives across Africa. <br /> <br /> At
+            DSA CORP, I work on projects that encourage creativity and personal
+            development, giving people the tools to explore new ideas and grow
+            beyond what they thought possible. My goal is straightforward: to
+            open doors for future thinkers, offer support to creative minds, and
+            inspire people to achieve their best in a connected, digital world.{" "}
+            <br /> <br /> If you’re interested in Web3, personal growth, or
+            creative work, I’d love for us to learn and grow together.
           </p>
         </Body>
 
