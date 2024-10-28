dffb7c87a209e3c2c15d3b50207a4411afe8bf97
3illBaby <thrillanon@outlook.com>
2024-10-28 20:34:33 +0100

    chore(projects.tsx): added title

--- app/components/projects.tsx
+++ app/components/projects.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Title from "@/app/components/title";
 
 const Projects = () => {
-  return <div className="h-screen">Projects</div>;
+  return (
+    <div className="flex h-screen flex-col pt-[5px] sm:pt-[200px] lg:pt-[600px]">
+      <Title title="Projects" />
+    </div>
+  );
 };
 
 export default Projects;
