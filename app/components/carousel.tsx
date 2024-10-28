40c65dff002e388271161913c072030462b18165
3illBaby <thrillanon@outlook.com>
2024-10-28 12:57:02 +0100

    refactor(carousel.tsx): refactored dummy content layout and added data
    \

--- app/components/carousel.tsx
+++ app/components/carousel.tsx
@@ -2,10 +2,11 @@
 import Image from "next/image";
 import React from "react";
 import { Carousel, Card } from "@/app/components/ui/apple-cards-carousel";
+import { dsa } from "@/app/assets";
 
 export function CarouselDemo() {
   const cards = data.map((card, index) => (
-    <Card key={card.src} card={card} index={index} />
+    <Card key={card.title} card={card} index={index} />
   ));
 
   return (
@@ -18,40 +19,32 @@ export function CarouselDemo() {
 const DummyContent = () => {
   return (
     <>
-      {[...new Array(3).fill(1)].map((_, index) => {
-        return (
-          <div
-            key={"dummy-content" + index}
-            className="mb-4 rounded-3xl bg-[#F5F5F7] p-8 dark:bg-neutral-800 md:p-14"
-          >
-            <p className="mx-auto max-w-3xl font-sans text-base text-neutral-600 dark:text-neutral-400 md:text-2xl">
-              <span className="font-bold text-neutral-700 dark:text-neutral-200">
-                The first rule of Apple club is that you boast about Apple club.
-              </span>{" "}
-              Keep a journal, quickly jot down a grocery list, and take amazing
-              class notes. Want to convert those notes to text? No problem.
-              Langotiya jeetu ka mara hua yaar is ready to capture every
-              thought.
-            </p>
-            <Image
-              src="https://assets.aceternity.com/macbook.png"
-              alt="Macbook mockup from Aceternity UI"
-              height="500"
-              width="500"
-              className="mx-auto h-full w-full object-contain md:h-1/2 md:w-1/2"
-            />
-          </div>
-        );
-      })}
+      <div className="mb-4 rounded-3xl bg-[#F5F5F7] p-8 dark:bg-neutral-800 md:p-14">
+        <p className="mx-auto max-w-3xl font-sans text-base text-neutral-600 dark:text-neutral-400 md:text-2xl">
+          <span className="font-bold text-neutral-700 dark:text-neutral-200">
+            The first rule of Apple club is that you boast about Apple club.
+          </span>{" "}
+          Keep a journal, quickly jot down a grocery list, and take amazing
+          class notes. Want to convert those notes to text? No problem.
+          Langotiya jeetu ka mara hua yaar is ready to capture every thought.
+        </p>
+        <Image
+          src="https://assets.aceternity.com/macbook.png"
+          alt="Macbook mockup from Aceternity UI"
+          height="500"
+          width="500"
+          className="mx-auto h-full w-full object-contain md:h-1/2 md:w-1/2"
+        />
+      </div>
     </>
   );
 };
 
 const data = [
   {
-    category: "Artificial Intelligence",
-    title: "You can do more with AI.",
-    src: "https://images.unsplash.com/photo-1593508512255-86ab42a8e620?q=80&w=3556&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    category: "Tech Innovator",
+    title: "Top Teki 100",
+    src: dsa,
     content: <DummyContent />,
   },
   {
