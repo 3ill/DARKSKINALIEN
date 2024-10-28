defba49fa8bf8811fe64a46861695781ef0505f2
3illBaby <thrillanon@outlook.com>
2024-10-28 20:34:16 +0100

    chore(carousel.tsx): replace dummy context with real data and modified styles

--- app/components/carousel.tsx
+++ app/components/carousel.tsx
@@ -2,7 +2,8 @@
 import Image from "next/image";
 import React from "react";
 import { Carousel, Card } from "@/app/components/ui/apple-cards-carousel";
-import { dsa } from "@/app/assets";
+import { darkskinalien, roundtable, roundtable3, top_teki } from "@/app/assets";
+import { CarouselData } from "@/app/data/constants";
 
 export function CarouselDemo() {
   const cards = data.map((card, index) => (
@@ -16,25 +17,22 @@ export function CarouselDemo() {
   );
 }
 
-const DummyContent = () => {
+interface ContentProps {
+  title: string;
+  context: string;
+}
+
+const DummyContent = ({ title, context }: ContentProps) => {
   return (
     <>
-      <div className="mb-4 rounded-3xl bg-[#F5F5F7] p-8 dark:bg-neutral-800 md:p-14">
-        <p className="mx-auto max-w-3xl font-sans text-base text-neutral-600 dark:text-neutral-400 md:text-2xl">
-          <span className="font-bold text-neutral-700 dark:text-neutral-200">
-            The first rule of Apple club is that you boast about Apple club.
+      <div className="glow mb-4 rounded-3xl bg-[#F5F5F7] p-8 dark:bg-neutral-800 md:p-14">
+        <p className="mx-auto max-w-3xl font-helvetica text-base capitalize text-neutral-600 dark:text-neutral-400 md:text-2xl">
+          <span className="font-helvetica font-bold uppercase text-neutral-700 dark:text-neutral-200">
+            {title}
           </span>{" "}
-          Keep a journal, quickly jot down a grocery list, and take amazing
-          class notes. Want to convert those notes to text? No problem.
-          Langotiya jeetu ka mara hua yaar is ready to capture every thought.
+          <br /> <br />
+          {context}
         </p>
-        <Image
-          src="https://assets.aceternity.com/macbook.png"
-          alt="Macbook mockup from Aceternity UI"
-          height="500"
-          width="500"
-          className="mx-auto h-full w-full object-contain md:h-1/2 md:w-1/2"
-        />
       </div>
     </>
   );
@@ -42,40 +40,47 @@ const DummyContent = () => {
 
 const data = [
   {
-    category: "Tech Innovator",
+    category: "TECH",
+    title: "RoundTable 3.0",
+    src: roundtable3,
+    content: (
+      <DummyContent
+        title={CarouselData.RoundTable3.title}
+        context={CarouselData.RoundTable3.context}
+      />
+    ),
+  },
+  {
+    category: "TECH",
+    title: "RoundTable 2.0.",
+    src: roundtable,
+    content: (
+      <DummyContent
+        title={CarouselData.RoundTable2.title}
+        context={CarouselData.RoundTable2.context}
+      />
+    ),
+  },
+  {
+    category: "TECH AWARD",
     title: "Top Teki 100",
-    src: dsa,
-    content: <DummyContent />,
+    src: top_teki,
+    content: (
+      <DummyContent
+        title={CarouselData.TopTeki.title}
+        context={CarouselData.TopTeki.context}
+      />
+    ),
   },
   {
-    category: "Productivity",
-    title: "Enhance your productivity.",
-    src: "https://images.unsplash.com/photo-1531554694128-c4c6665f59c2?q=80&w=3387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    content: <DummyContent />,
-  },
-  {
-    category: "Product",
-    title: "Launching the new Apple Vision Pro.",
-    src: "https://images.unsplash.com/photo-1713869791518-a770879e60dc?q=80&w=2333&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    content: <DummyContent />,
-  },
-
-  {
-    category: "Product",
-    title: "Maps for your iPhone 15 Pro Max.",
-    src: "https://images.unsplash.com/photo-1599202860130-f600f4948364?q=80&w=2515&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    content: <DummyContent />,
-  },
-  {
-    category: "iOS",
-    title: "Photography just got better.",
-    src: "https://images.unsplash.com/photo-1602081957921-9137a5d6eaee?q=80&w=2793&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    content: <DummyContent />,
-  },
-  {
-    category: "Hiring",
-    title: "Hiring for a Staff Software Engineer",
-    src: "https://images.unsplash.com/photo-1511984804822-e16ba72f5848?q=80&w=2048&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    content: <DummyContent />,
+    category: "WEB 3.0",
+    title: "Tech Nxt 2.0",
+    src: darkskinalien,
+    content: (
+      <DummyContent
+        title={CarouselData.TechNxt.title}
+        context={CarouselData.TechNxt.context}
+      />
+    ),
   },
 ];
