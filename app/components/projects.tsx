1b133100c55dbe2ae678a263385282f7461f885b
3illBaby <thrillanon@outlook.com>
2024-11-12 10:12:17 +0100

    style(projects.tsx): added padding

--- app/components/projects.tsx
+++ app/components/projects.tsx
@@ -4,7 +4,7 @@ import { ProjectsCard } from "@/app/components/projects-card";
 
 const Projects = () => {
   return (
-    <div className="flex h-screen flex-col pt-[50px] sm:pt-[200px] lg:pt-[600px] mb-[1000px]">
+    <div className="flex h-screen flex-col pt-[50px] sm:pt-[250px] lg:pt-[600px] mb-[1000px]">
       <Title title="Projects" />
       <div className={`flex flex-row px-5`}>
         <ProjectsCard />
