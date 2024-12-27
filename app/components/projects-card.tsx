8f2b9d4f144e5ca1c0cc46f823b566ddf7d23e61
3illBaby <thrillanon@outlook.com>
2024-12-27 20:07:28 +0100

    chore(projects-card.tsx): made component dynamic

--- app/components/projects-card.tsx
+++ app/components/projects-card.tsx
@@ -1,13 +1,21 @@
 "use client";
 
-import Image from "next/image";
+import Image, { StaticImageData } from "next/image";
 import React from "react";
 import { CardBody, CardContainer, CardItem } from "@/app/components/ui/3d-card";
 import { IconFolderShare } from "@tabler/icons-react";
-import { hbb } from "@/app/assets";
 import { useTheme } from "@/app/context/theme.context";
+import Link from 'next/link';
 
-export function ProjectsCard() {
+interface ProjectProps {
+  id: number;
+  duration: string;
+  name: string;
+  role: string;
+  image: StaticImageData;
+}
+
+export function ProjectsCard({id, duration, name, role, image}: ProjectProps) {
   const { theme } = useTheme();
   return (
     <CardContainer className="inter-var">
@@ -16,11 +24,11 @@ export function ProjectsCard() {
           translateZ="50"
           className="font-medium text-neutral-600 dark:text-primary_light"
         >
-          Nov 23 - Dec 24
+          {duration}
         </CardItem>
         <CardItem translateZ="100" className="mt-4 w-full">
           <Image
-            src={hbb}
+            src={image}
             height="1000"
             width="1000"
             className="h-80 w-full rounded-xl object-cover object-center group-hover/card:shadow-xl"
@@ -30,20 +38,23 @@ export function ProjectsCard() {
         <div className="mt-20 flex items-center justify-between">
           <div className={`flex flex-row items-center gap-1`}>
             <CardItem translateZ={-20} translateX={-40}>
+              <Link key={id} href={`/projects/${id}`}>
               <IconFolderShare
                 color={theme === "light" ? " #404040" : "#d4d4d4"}
                 width={70}
                 height={70}
+                className={`animate-pulse duration-500 transition-all`}
               />
+              </Link>
             </CardItem>
             <div className={`flex flex-col`}>
               <CardItem
                 as="p"
                 translateZ={-20}
                 translateX={-40}
-                className="mt-2 max-w-sm text-lg uppercase text-neutral-700 accent-neutral-700 dark:text-neutral-300"
+                className="mt-2 max-w-sm text-lg font-extrabold uppercase text-neutral-700 accent-neutral-700 dark:text-neutral-300"
               >
-                Heads by bnxn
+                {name}
               </CardItem>
               <CardItem
                 as="p"
@@ -51,7 +62,7 @@ export function ProjectsCard() {
                 translateX={-40}
                 className="max-w-sm text-[15px] capitalize text-neutral-500 accent-neutral-300 dark:text-neutral-300"
               >
-                Community Engr
+                {role}
               </CardItem>
             </div>
           </div>
