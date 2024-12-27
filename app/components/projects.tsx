614330fba35b5a8c8fe888fe02f356b726d62767
3illBaby <thrillanon@outlook.com>
2024-12-27 20:14:27 +0100

    chore(projects.tsx): added props

--- app/components/projects.tsx
+++ app/components/projects.tsx
@@ -1,15 +1,20 @@
 import React from "react";
 import Title from "@/app/components/title";
 import { ProjectsCard } from "@/app/components/projects-card";
+import { ProjectsData } from "@/app/data/constants";
 
 const Projects = () => {
   return (
-    <div className="flex h-screen flex-col pt-[50px] sm:pt-[250px] lg:pt-[600px] mb-[1000px]">
+    <section id={`projects`} className="flex h-screen flex-col mt-[50px] pt-[50px] sm:pt-[250px] lg:pt-[600px] mb-[1000px]">
       <Title title="Projects" />
       <div className={`flex flex-row px-5`}>
-        <ProjectsCard />
+        {ProjectsData.map((project) => (
+          <div key={project.id} className={`flex flex-row w-fit`}>
+            <ProjectsCard id={project.id} duration={project.duration} name={project.name} role={project.role} image={project.image} />
+          </div>
+        ))}
       </div>
-    </div>
+    </section>
   );
 };
 
