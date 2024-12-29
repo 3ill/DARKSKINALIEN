f5a32238f8b243a1302b0c7356a24cdb7a146fa1
3illBaby <thrillanon@outlook.com>
2024-12-29 19:49:32 +0100

    chore(store.interface.ts): iServiceCard

--- app/store/interface/store.interface.ts
+++ app/store/interface/store.interface.ts
@@ -1,4 +1,18 @@
+import React from "react";
+
 export interface IMenuToggled {
   isToggled: boolean;
   setToggled: () => void;
+}
+
+export interface IServiceCard {
+  data: {
+    description: string;
+    title: string;
+    src: string;
+    ctaText: string;
+    ctaText2: string;
+    ctaLink: string;
+    content: () => React.JSX.Element;
+  }[];
 }
