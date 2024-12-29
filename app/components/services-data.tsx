75777cb163803ffb4e0dc0c8b14e02e895fd225d
3illBaby <thrillanon@outlook.com>
2024-12-29 20:43:20 +0100

    chore(service-data.tsx): added community management data card

--- app/components/services-data.tsx
+++ app/components/services-data.tsx
@@ -74,3 +74,54 @@ export const PublicSpeakingCards = [
   },
 
 ];
+
+
+export const CommunityManagementCard = [
+  {
+    description: "HeadsByBnxn",
+    title: "Web3 Communities",
+    src: "https://assets.aceternity.com/demos/metallica.jpeg",
+    ctaText: "explore",
+    ctaText2: "book",
+    ctaLink: "/",
+    content: () => {
+      return (
+        <p>
+          Metallica, an iconic American heavy metal band, is renowned for their
+          powerful sound and intense performances that resonate deeply with
+          their audience. Formed in Los Angeles, California, they have become a
+          cultural icon in the heavy metal music industry. <br /> <br /> Their
+          songs often reflect themes of aggression, social issues, and personal
+          struggles, capturing the essence of the heavy metal genre. With a
+          career spanning over four decades, Metallica has released numerous hit
+          albums and singles that have garnered them a massive fan following
+          both in the United States and abroad.
+        </p>
+      );
+    },
+
+  },
+  {
+    description: "Paal AI",
+    title: "AI Communities",
+    src: "https://assets.aceternity.com/demos/metallica.jpeg",
+    ctaText: "explore",
+    ctaText2: "book",
+    ctaLink: "https://ui.aceternity.com/templates",
+    content: () => {
+      return (
+        <p>
+          Metallica, an iconic American heavy metal band, is renowned for their
+          powerful sound and intense performances that resonate deeply with
+          their audience. Formed in Los Angeles, California, they have become a
+          cultural icon in the heavy metal music industry. <br /> <br /> Their
+          songs often reflect themes of aggression, social issues, and personal
+          struggles, capturing the essence of the heavy metal genre. With a
+          career spanning over four decades, Metallica has released numerous hit
+          albums and singles that have garnered them a massive fan following
+          both in the United States and abroad.
+        </p>
+      );
+    },
+  }
+]
