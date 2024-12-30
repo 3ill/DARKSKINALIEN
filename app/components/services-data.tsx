6d3ada43b09dc2b56419a9c5a3be9a275ed63ea6
3illBaby <thrillanon@outlook.com>
2024-12-30 17:33:47 +0100

    chore(services-data.tsx): added static data for services

--- app/components/services-data.tsx
+++ app/components/services-data.tsx
@@ -1,5 +1,12 @@
 import React from "react";
-import { limitless_party, metaverse_conf, ypit_mindset } from "@/app/assets";
+import {
+  btf,
+  dsa_bnxn,
+  limitless_party,
+  metaverse_conf,
+  paal,
+  ypit_mindset,
+} from "@/app/assets";
 
 export const PublicSpeakingData = [
   {
@@ -12,16 +19,14 @@ export const PublicSpeakingData = [
     content: () => {
       return (
         <p>
-          Lana Del Rey, an iconic American singer-songwriter, is celebrated for
-          her melancholic and cinematic music style. Born Elizabeth Woolridge
-          Grant in New York City, she has captivated audiences worldwide with
-          her haunting voice and introspective lyrics. <br /> <br /> Her songs
-          often explore themes of tragic romance, glamour, and melancholia,
-          drawing inspiration from both contemporary and vintage pop culture.
-          With a career that has seen numerous critically acclaimed albums, Lana
-          Del Rey has established herself as a unique and influential figure in
-          the music industry, earning a dedicated fan base and numerous
-          accolades.
+          Whether you&apos;re planning an elegant corporate gala, a tech event,
+          or a lively product launch, a skilled MC can transform your event into
+          an unforgettable experience. <br /> <br /> As your professional Event
+          Host, I bring energy, charisma, and seamless flow to your event,
+          ensuring everything runs smoothly and keeps your guests engaged from
+          start to finish.
+          <br /> <br /> Ready to bring your vision to life? Book a consultation
+          today, and let&apos;s get started.
         </p>
       );
     },
@@ -36,15 +41,16 @@ export const PublicSpeakingData = [
     content: () => {
       return (
         <p>
-          Babu Maan, a legendary Punjabi singer, is renowned for his soulful
-          voice and profound lyrics that resonate deeply with his audience. Born
-          in the village of Khant Maanpur in Punjab, India, he has become a
-          cultural icon in the Punjabi music industry. <br /> <br /> His songs
-          often reflect the struggles and triumphs of everyday life, capturing
-          the essence of Punjabi culture and traditions. With a career spanning
-          over two decades, Babu Maan has released numerous hit albums and
-          singles that have garnered him a massive fan following both in India
-          and abroad.
+          In a world where technology is evolving faster than ever, staying
+          ahead of the curve is crucial. <br /> <br /> As a passionate advocate
+          and expert in emerging technologies, I offer engaging Tech Talk
+          sessions that dive deep into the innovations shaping our future.
+          <br /> <br />
+          From AI and blockchain to cybersecurity and IoT, my talks provide your
+          audience with the insights, trends, and practical knowledge they need
+          to navigate today’s tech landscape.
+          <br /> <br /> Ready to bring your vision to life? Book a consultation
+          today, and let&apos;s get started.
         </p>
       );
     },
@@ -60,94 +66,100 @@ export const PublicSpeakingData = [
     content: () => {
       return (
         <p>
-          Metallica, an iconic American heavy metal band, is renowned for their
-          powerful sound and intense performances that resonate deeply with
-          their audience. Formed in Los Angeles, California, they have become a
-          cultural icon in the heavy metal music industry. <br /> <br /> Their
-          songs often reflect themes of aggression, social issues, and personal
-          struggles, capturing the essence of the heavy metal genre. With a
-          career spanning over four decades, Metallica has released numerous hit
-          albums and singles that have garnered them a massive fan following
-          both in the United States and abroad.
+          We all have the power to achieve more, but sometimes we just need a
+          push to tap into our full potential. <br /> <br /> Whether it’s
+          overcoming challenges, pursuing dreams, or fostering resilience, my
+          Motivational Talks are designed to inspire and empower your audience
+          to take action and achieve greatness. <br /> <br /> With a blend of
+          personal stories, practical advice, and high-energy delivery, I help
+          individuals and teams break through barriers and move toward their
+          goals with renewed purpose and confidence.
+          <br /> <br /> Ready to bring your vision to life? Book a consultation
+          today, and let&apos;s get started.
         </p>
       );
     },
   },
-
 ];
 
 export const CommunityManagementData = [
   {
     description: "HeadsByBnxn",
     title: "Web3 Communities",
-    src: "https://assets.aceternity.com/demos/metallica.jpeg",
+    src: dsa_bnxn,
     ctaText: "explore",
     ctaText2: "book",
     ctaLink: "/",
     content: () => {
       return (
         <p>
-          Metallica, an iconic American heavy metal band, is renowned for their
-          powerful sound and intense performances that resonate deeply with
-          their audience. Formed in Los Angeles, California, they have become a
-          cultural icon in the heavy metal music industry. <br /> <br /> Their
-          songs often reflect themes of aggression, social issues, and personal
-          struggles, capturing the essence of the heavy metal genre. With a
-          career spanning over four decades, Metallica has released numerous hit
-          albums and singles that have garnered them a massive fan following
-          both in the United States and abroad.
+          In the fast-paced and evolving world of Web3, building a vibrant and
+          engaged community is not just important—it’s essential. <br /> <br />
+          Whether you’re launching an NFT project, a decentralized finance
+          (DeFi) platform, or a blockchain-based initiative, a loyal and active
+          community is the foundation of success. <br /> <br /> With my
+          Community Management services, I help you foster genuine connections,
+          drive engagement, and create a sustainable ecosystem that supports
+          long-term growth.
+          <br /> <br /> Ready to bring your vision to life? Book a consultation
+          today, and let&apos;s get started.
         </p>
       );
     },
-
   },
   {
     description: "Paal AI",
     title: "AI Communities",
-    src: "https://assets.aceternity.com/demos/metallica.jpeg",
+    src: paal,
     ctaText: "explore",
     ctaText2: "book",
-    ctaLink: "https://ui.aceternity.com/templates",
+    ctaLink: "/",
     content: () => {
       return (
         <p>
-          Metallica, an iconic American heavy metal band, is renowned for their
-          powerful sound and intense performances that resonate deeply with
-          their audience. Formed in Los Angeles, California, they have become a
-          cultural icon in the heavy metal music industry. <br /> <br /> Their
-          songs often reflect themes of aggression, social issues, and personal
-          struggles, capturing the essence of the heavy metal genre. With a
-          career spanning over four decades, Metallica has released numerous hit
-          albums and singles that have garnered them a massive fan following
-          both in the United States and abroad.
+          In the ever-evolving world of Artificial Intelligence, building and
+          maintaining a thriving community is key to fostering innovation,
+          collaboration, and real-world impact.
+          <br /> <br /> With my AI Community Management services, I help you
+          connect with thought leaders, developers, enthusiasts, and users who
+          are as passionate about AI as you are. <br /> <br /> By leveraging the
+          latest trends and technologies, I create meaningful experiences that
+          drive engagement, accelerate growth, and turn your community into a
+          powerful force for your brand’s success.
+          <br /> <br /> Ready to bring your vision to life? Book a consultation
+          today, and let&apos;s get started.
         </p>
       );
     },
-  }
-]
+  },
+];
 
 export const ProjectManagementData = [
   {
     description: "Building The Future",
     title: "Campaign",
-    src: "https://assets.aceternity.com/demos/metallica.jpeg",
+    src: btf,
     ctaText: "explore",
     ctaText2: "book",
     ctaLink: "/",
     content: () => {
       return (
         <p>
-          Metallica, an iconic American heavy metal band, is renowned for their
-          powerful sound and intense performances that resonate deeply with
-          their audience. Formed in Los Angeles, California, they have become a
-          cultural icon in the heavy metal music industry. <br /> <br /> Their
-          songs often reflect themes of aggression, social issues, and personal
-          struggles, capturing the essence of the heavy metal genre. With a
-          career spanning over four decades, Metallica has released numerous hit
-          albums and singles that have garnered them a massive fan following
-          both in the United States and abroad.
+          Managing tech initiatives, educational programs, and motivational
+          campaigns requires precision, strategic thinking, and seamless
+          execution.
+          <br /> <br /> I specialize in taking your vision and turning it into a
+          reality, ensuring every detail is handled from start to finish. With a
+          focus on efficiency, effective resource allocation, and clear
+          communication, I manage the entire process, keeping everything on
+          track and aligned with your goals.
+          <br /> <br /> From initial planning to final delivery, I’m here to
+          ensure your project stays on time, on budget, and exceeds
+          expectations.
+          <br /> <br /> Ready to bring your vision to life? Book a consultation
+          today, and let&apos;s get started.
         </p>
       );
     },
-  }
-]
+  },
+];
