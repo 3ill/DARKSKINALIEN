0f790234f723ae3ebf6989ab44a09ba0f0579974
3illBaby <thrillanon@outlook.com>
2024-12-30 19:29:31 +0100

    chore(project-management.tsx): added style highlight

--- app/components/project-management.tsx
+++ app/components/project-management.tsx
@@ -10,14 +10,20 @@ const ProjectManagement = () => {
   return (
     <section className={`services_container !lg:pt-[150px] !pt-[200px]`}>
       <div className={`flex w-full`}>
-        <motion.h3
+        <motion.div
           variants={softVariant2(0.75, 0.5)}
           initial={`hidden`}
           whileInView={`visible`}
-          className={`services_title`}
+          className={`flex w-full items-center gap-1`}
         >
-          Project <br /> Management
-        </motion.h3>
+          <div
+            className={`h-[200px] w-1 rounded-md bg-gradient-to-r from-green-400 to-green-600`}
+          />
+
+          <h3 className={`services_title`}>
+            Project <br /> Management
+          </h3>
+        </motion.div>
       </div>
       <div className={`service_component_wrapper`}>
         <ServiceCard data={ProjectManagementData} />
