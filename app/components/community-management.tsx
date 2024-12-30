f16eee808e47dd994b4e462c02d696a054ae3919
3illBaby <thrillanon@outlook.com>
2024-12-30 19:28:30 +0100

    refactor(communiity-management .tsx): modified component style

--- app/components/community-management.tsx
+++ app/components/community-management.tsx
@@ -8,14 +8,19 @@ const CommunityManagement = () => {
   return (
     <section className={`services_container !lg:pt-[150px] !pt-[200px]`}>
       <div className={`flex w-full`}>
-        <motion.h3
+        <motion.div
           variants={softVariant2(0.75, 0.5)}
           initial={`hidden`}
           whileInView={`visible`}
-          className={`services_title`}
+          className={`flex w-full items-center gap-1`}
         >
-          Community <br /> Management
-        </motion.h3>
+
+          <div className={`w-1 h-[100px] sm:h-[200px] rounded-md bg-gradient-to-r from-yellow-400 to-yellow-600`}/>
+
+          <motion.h3 className={`services_title`}>
+            Community <br /> Management
+          </motion.h3>
+        </motion.div>
       </div>
       <div className={`service_component_wrapper`}>
         <ServiceCard data={CommunityManagementData} />
