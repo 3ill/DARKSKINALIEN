d85805cd851a97966ed998cc95412653340a93f6
3illBaby <thrillanon@outlook.com>
2025-01-02 08:53:04 +0100

    refactor(services.tsx): used title componenet

--- app/sections/services.tsx
+++ app/sections/services.tsx
@@ -1,10 +1,11 @@
 "use client";
 import React from "react";
 import { motion } from "framer-motion";
-import { staggerContainer, textVariant2 } from "@/app/utils/motion";
+import { staggerContainer } from "@/app/utils/motion";
 import PublicSpeaking from "@/app/components/public-speaking";
 import CommunityManagement from "@/app/components/community-management";
 import ProjectManagement from "@/app/components/project-management";
+import Title from "../components/title";
 
 const Services = () => {
   return (
@@ -16,21 +17,7 @@ const Services = () => {
       id={`services`}
       className={`mb-[100px] flex min-h-screen w-full flex-col pt-[450px] lg:pt-[800px]`}
     >
-      <motion.div
-        variants={textVariant2}
-        initial={`hidden`}
-        whileInView={`show`}
-        className={`glow flex flex-row items-center gap-1`}
-      >
-        <div
-          className={`h-9 w-1 rounded-md bg-primary_dark dark:bg-primary_light`}
-        />
-        <p
-          className={`text_variant2 font-Azeret text-[24px] font-extrabold uppercase sm:text-[40px]`}
-        >
-          Services
-        </p>
-      </motion.div>
+      <Title title="Services" />
       <PublicSpeaking />
       <CommunityManagement />
       <ProjectManagement />
