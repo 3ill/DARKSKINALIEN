ff859ee0e1cc39553cb7d5414726f4f08ecac167
3illBaby <thrillanon@outlook.com>
2025-01-04 21:39:00 +0100

    chore(project.tsx): created header and logo styles and container

--- app/projects/[id]/page.tsx
+++ app/projects/[id]/page.tsx
@@ -1,3 +1,5 @@
+import { ProjectsData } from "@/app/data/constants";
+import Image, { StaticImageData } from "next/image";
 import React from "react";
 
 interface ProjectPageProps {
@@ -6,11 +8,26 @@ interface ProjectPageProps {
   };
 }
 
-const ProjectPage = ({params}: ProjectPageProps) => {
+const ProjectPage = ({ params }: ProjectPageProps) => {
+  const project = ProjectsData.find(
+    (project) => project.id.toString() === params.id,
+  );
   return (
-    <div>
-      <h1 className={`text-white`}>{params.id}</h1>
-    </div>
+    <section className={`flex h-screen w-full flex-col`}>
+      <div className={`project_header_container relative`}>
+        <h1 className="font-helvetica text-[35px] font-extrabold uppercase text-neutral-50 sm:text-[62px]">
+          {project?.name}
+        </h1>
+
+        <div className={`project_logo_container`}>
+          <Image
+            src={project?.image as StaticImageData}
+            alt={project?.name as string}
+            className={`inset-1/2] h-fit w-[100px] rounded-full object-contain object-center sm:w-[150px] lg:w-[200px]`}
+          />
+        </div>
+      </div>
+    </section>
   );
 };
 
